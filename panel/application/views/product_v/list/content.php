9c21cf614e145196e6cb2a9026611539e19bd36a
serhatskywalker <serhatkrbyk1@icloud.com>
2022-10-12 10:18:58 +0300

    ürünler için arayüz eklendi

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,72 @@
-ürün content
+<div class="row">
+	<div class="col-md-12">
+		<h4 class="m-b-lg">Ürün Listesi</h4>
+	</div><!-- END column -->
+	<div class="col-md-12">
+		<div class="widget p-lg">
+
+			<table class="table table-hover table-striped">
+				<thead>
+				<th>#id</th>
+				<th>url</th>
+				<th>Başlik</th>
+				<th>Açıklama</th>
+				<th>Durumu</th>
+				<th>İşlem</th>
+				</thead>
+				<tbody>
+				<tr>
+					<td>#1</td>
+					<td>monitor-askisi</td>
+					<td>Monitor Askisi</td>
+					<td>360 dere kullanılabilen monitör askısıdır</td>
+					<td>1/0</td>
+					<td>
+						<a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
+						<a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
+					</td>
+				</tr>
+				<tr>
+					<td>#1</td>
+					<td>monitor-askisi</td>
+					<td>Monitor Askisi</td>
+					<td>360 dere kullanılabilen monitör askısıdır</td>
+					<td>1/0</td>
+					<td>
+						<a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
+						<a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
+					</td>
+				</tr>
+				<tr>
+					<td>#1</td>
+					<td>monitor-askisi</td>
+					<td>Monitor Askisi</td>
+					<td>360 dere kullanılabilen monitör askısıdır</td>
+					<td>1/0</td>
+					<td>
+						<a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
+						<a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
+					</td>
+				</tr>
+				<tr>
+					<td>#1</td>
+					<td>monitor-askisi</td>
+					<td>Monitor Askisi</td>
+					<td>360 dere kullanılabilen monitör askısıdır</td>
+					<td>1/0</td>
+					<td>
+						<a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
+						<a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
+					</td>
+				</tr>
+				</tbody>
+
+			</table>
+		</div><!-- .widget -->
+	</div><!-- END column -->
+
+</div>
+<!--<tr><th>#</th><th>First Name</th><th>Last Name</th><th>Username</th></tr>-->
+<!--<tr><td>1</td><td>Mark</td><td>Otto</td><td>@mdo</td></tr>-->
+<!--<tr><td>2</td><td>Jacob</td><td>Thornton</td><td>@fat</td></tr>-->
+<!--<tr><td>3</td><td>Larry</td><td>the Bird</td><td>@twitter</td></tr>-->
