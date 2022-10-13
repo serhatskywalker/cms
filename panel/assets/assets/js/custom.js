64cba7252ebf465bcc88088233147abd15bc0249
serhatskywalker <serhatkrbyk1@icloud.com>
2022-10-13 13:22:53 +0300

    SweetAlert ile kayıt silme islemi gerceklesti,

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,9 +1,23 @@
 $(document).ready(function () {
-	Swal.fire({
-		title: 'Error!',
-		text: 'Do you want to continue',
-		icon: 'error',
-		confirmButtonText: 'Cool'
+
+	$(".remove-btn").click(function (e){
+
+		var $data_url = $(this).data("url");
+
+		Swal.fire({
+			title: 'Emin misiniz?',
+			text: "By işlemi geri alamayacaksınız!",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Evet, Sil!',
+			cancelButtonText: "Hayır"
+		}).then(function (result) {
+			if (result.value) {
+			window.location.href = $data_url;
+			}
+		})
 	})
 
 })
