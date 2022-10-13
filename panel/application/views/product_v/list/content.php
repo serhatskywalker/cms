ad2c522f42ec7a5bda6e100426759591856c7f1e
serhatskywalker <serhatkrbyk1@icloud.com>
2022-10-13 13:48:09 +0300

    isActive ile urunler

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -38,6 +38,8 @@
 						<td>
 
 							<input
+									data-url="<?php echo base_url("product/isActiveSetter/$item->id"); ?>"
+									class="isActive"
 									type="checkbox"
 									data-switchery
 									data-color="#10c469"
--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-	$(".remove-btn").click(function (e){
+	$(".remove-btn").click(function (){
 
 		var $data_url = $(this).data("url");
 
@@ -20,6 +20,15 @@ $(document).ready(function () {
 		})
 	})
 
+	$(".isActive").change(function (){
+		var $data = $(this).prop("checked");
+		var $data_url = $(this).data("url");
+
+		if(typeof $data !== "undefined" && typeof  $data_url !== "undefined"){
+			$.post($data_url, {data : $data}, function (response) {
+			});
+		}
+ 	})
 })
 
 
